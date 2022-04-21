6d96837ecd5c4f2dca506c9b612093b5d7c92f21
nmartoia <nmartoia@edenschool.fr>
2022-04-21 13:42:47 +0200

    add css and js

--- js/app.js
+++ js/app.js
@@ -3,15 +3,20 @@ let div=document.querySelector("#div");
 function addelement(){
     let p =document.createElement("p");
     let box = document.createElement('div')
+    let up = document.createElement('button')
+    let down = document.createElement('button')
+    up.textContent='UP'
+    down.textContent='DOWN'
     box.className='box'
     div.appendChild(box)
     box.appendChild(p)
+    box.appendChild(up)
+    box.appendChild(down)
     fetch('https://api.kanye.rest/').then((response)=>{
-    console.log(response);
     return response.json()
 }).then((data)=>{
-    console.log(data.quote)
     p.textContent=data.quote;
 }).catch()
 }
 btn.addEventListener('click',addelement)
+//p.previousElementSibling
